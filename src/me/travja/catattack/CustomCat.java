5598098bdc633a24ff4f7dc59b0ee49e37febbaa
Travja <the.only.t.craft@gmail.com>
2020-02-27 15:59:46 -0700

    Initial commit! V1

---
+++ src/me/travja/catattack/CustomCat.java
@@ -0,0 +1,35 @@
+package me.travja.catattack;
+
+import net.minecraft.server.v1_8_R3.*;
+import org.bukkit.craftbukkit.v1_8_R3.util.UnsafeList;
+
+import java.lang.reflect.Field;
+
+
+public class CustomCat extends EntityOcelot {
+
+    public CustomCat() {
+        super(null);
+    }
+
+    public CustomCat(World world) {
+        super(world);
+        try {
+            Field bField = PathfinderGoalSelector.class.getDeclaredField("b");
+            bField.setAccessible(true);
+            Field cField = PathfinderGoalSelector.class.getDeclaredField("c");
+            cField.setAccessible(true);
+            bField.set(targetSelector, new UnsafeList<PathfinderGoalSelector>());
+            cField.set(targetSelector, new UnsafeList<PathfinderGoalSelector>());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
+        this.targetSelector.a();
+        this.targetSelector.a(1, new PathfinderGoalOwnerHurtByTarget(this));
+        this.targetSelector.a(2, new PathfinderGoalOwnerHurtTarget(this));
+        this.targetSelector.a(3, new PathfinderGoalHurtByTarget(this, true, new Class[0]));
+        this.targetSelector.a(4, new PathfinderGoalRandomTargetNonTamed(this, EntityChicken.class, false, null));
+    }
+}
